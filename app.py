65f859243a8464e8ac2846c5e45cb611956caa0b
matvejcekasov <chekashovmatvey@gmail.com>
2024-04-15 19:47:37 +0300

    Тестовый коммит для объяснения работы веток

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World! I have some cool music!'
 
+
 if __name__ == '__main__':
+    # Я в ветке register!
     app.run()
